a41668ab300c4d1faae1225a9090db1414a79a95
Gabriel Ramos <gabrielrs.sh@gmail.com>
2022-01-13 00:38:29 -0300

    coleção de constantes de nome de classes base

---
+++ src/br/edu/ufcspa/model/ClassName.java
@@ -0,0 +1,27 @@
+package br.edu.ufcspa.model;
+
+public final class ClassName {
+
+    private ClassName(){};
+
+    public static final String TRANSFER = "Transfer";
+
+    public static final String PATHOLOGICALDISPOSITION = "PathologicalDisposition";
+
+    public static final String GEOGRAPHICENTITY = "GeographicEntity";
+
+    public static final String ORGANISM = "Organism";
+
+    public static final String VERTEBRATE = "Vertebrate";
+
+    public static final String HUMAN = "Human";
+
+    public static final String VIRUS = "Virus";
+
+    public static final String BRAZILOCATION = "BrazilLocation";
+
+    public static final String PATHOGENTRANSFERBYVECTOR = "PathogenTransferByVector";
+
+
+
+}
